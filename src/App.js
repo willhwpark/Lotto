445faa0badfece0a77827e8cac7350206eb2ae45
William Park <89389554+willhwpark@users.noreply.github.com>
2023-02-28 18:39:28 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+
+  const [num, setNum] = useState(0);
+
+  function Lottery() {
+      let winningNumber = [];
+      for (let i = 0; i < 7; i++) {
+          let numberGenerator = Math.floor(Math.random() * 45) + 1;
+          let noDuplicate = winningNumber.includes(numberGenerator);
+          if (!noDuplicate) {
+              winningNumber.push(numberGenerator);
+          } else {
+              while (noDuplicate) {
+                  numberGenerator = Math.floor(Math.random() * 45) + 1;
+                  noDuplicate = winningNumber.includes(numberGenerator);
+                  if (!noDuplicate) {
+                      winningNumber.push(numberGenerator);
+                  }
+              }
+          }
+      }
+      winningNumber.sort(function (a,b) {
+          if(a > b) return 1;
+          if(a < b) return -1;
+      });
+      console.log(winningNumber);
+      return winningNumber;
+  }
+  const handleClick = () => {
+      setNum(Lottery());
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>World Wide Lottery</h1>
+      <h1>Winning Number</h1>
+      <h1>{num + " "}</h1>
+      <button onClick={handleClick}>Generate</button>
     </div>
   );
 }
